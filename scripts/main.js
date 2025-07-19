6003c7a81b85137fe67965b41273e61ac409983d
Beluszky Aranyköpései <192024038+beluszky-aranykopesei@users.noreply.github.com>
2025-07-19 13:05:07 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
   window.onload = function() {
-    document.getElementById("beluszky-popup").style.display = "block";
+    document.getElementById("beluszky-regi-popup").style.display = "block";
 
     // Opció: automatikus eltűnés 10 másodperc után
     setTimeout(function() {
